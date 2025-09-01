054dbb810bef4e774b5c247d4054ebf79841684c
I Putu Wira Budhi Guna Ariyasa <wirbud1134@gmail.com>
2025-09-01 15:03:33 +0800

    fix: conditionally render admin sidebar based on route
    - Added a conditional check to render the admin sidebar only if the current route is not part of the settings section, improving layout flexibility.

--- resources/views/components/layouts/admin.blade.php
+++ resources/views/components/layouts/admin.blade.php
@@ -1,7 +1,9 @@
 <x-layouts.app :title="$title ?? null">
     <div class="flex min-h-screen">
         <!-- Sidebar -->
+        @if (!request()->routeIs('settings.*'))
         <x-layouts.admin.sidebar />
+        @endif
 
         <!-- Main Content -->
         <div class="flex-1 flex flex-col">
